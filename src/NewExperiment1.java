ed6fd3d8e7200fc4997f011dfad36ec81769ce12
Mohan2 <umohantest1@gmail.com>
2023-04-17 11:42:45 +0530

    Experiment 1.2

--- src/NewExperiment1.java
+++ src/NewExperiment1.java
@@ -3,6 +3,10 @@ public class NewExperiment1 {
 	
 	public static void main(String[] args) {
 		System.out.println("My experiment 1.1");
+		
+		// Mohan 2
+		
+		System.out.println("Experiment 1.2");
 	}
 
 }
